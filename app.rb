0a3afb6c9a0cee9f41ce7600661eba2e6d75b340
sdzubiria <34401013+sdzubiria@users.noreply.github.com>
2024-04-17 21:35:23 +0000

    Final draft 2

--- app.rb
+++ app.rb
@@ -5,40 +5,54 @@ get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+get("/add") do
   erb(:add_form)
 end
 
 get("/wizard_add") do
-  @first_num = params.fetch("first_num").to_f
-
+  @first_num = params.fetch("first_number").to_f
+  @second_num = params.fetch("second_number").to_f
   @result = @first_num + @second_num
   erb(:add_result)
 end
 
 get("/subtract") do
-  erb(:sub_form)
+  erb(:subtraction_form)
 end
 
 get("/wizard_subtract") do
   @first_num = params.fetch("first_num").to_f
   @second_num = params.fetch("second_num").to_f
-
+  @result = @first_num - @second_num
   erb(:sub_result)
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
-  erb(:mult_result)
+get("/wizard_multiply") do
+  @first_number = params.fetch("first_number").to_f
+  @second_num = params.fetch("second_number").to_f
+  @result = @first_number * @second_num
+  erb(:mult_results)
 end
 
 get("/divide") do
-  erb(:div_form)
+  erb(:divform)
 end
 
 get("/wizard_divide") do
+  @first_number = params.fetch("first_number").to_f
+  @second_num = params.fetch("second_num").to_f
+  @result = @second_num.zero? ? "Cannot divide by zero" : @first_number / @second_num
   erb(:div_result)
 end
+
+get("/wizard_multiply") do
+  @first_number = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+  @result = @first_number * @second_num
+  erb(:mult_results)
+end
+
